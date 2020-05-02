bc925c94170e77ebb8926d85eb538044b08bc86d
Asus <Asus@DESKTOP-RHCO5U6>
2020-05-02 12:27:56 +0300

    Added Ex2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            int z = x + y;
+
+            Console.WriteLine(z);
+
+            Console.ReadKey();
+        }
+    }
+}
