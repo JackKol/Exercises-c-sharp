103e79f9342ad4956128f63f1617c42f89eaa635
Asus <Asus@DESKTOP-RHCO5U6>
2020-05-02 14:59:47 +0300

    Ex4-Ex11

---
+++ Exercise11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.Write("Enter your age ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look younger than {0} ", age);
+            Console.Read();
+        }
+    }
+}
